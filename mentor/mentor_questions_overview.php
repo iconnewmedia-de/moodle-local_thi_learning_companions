1e57a34c1ff37ec63a60f759eb514ada3a9fb0fb
Ruhnke <kevin.ruhnke@iconnewmedia.de>
2023-02-28 11:09:55 +0100

    Updated the Question Overview for Users and Mentors

--- mentor/mentor_questions_overview.php
+++ mentor/mentor_questions_overview.php
@@ -1,6 +1,7 @@
 <?php
-require_once '../../../config.php';
-require_once '../lib.php';
+
+require_once dirname(__DIR__, 3).'/config.php';
+require_once dirname(__DIR__).'/lib.php';
 
 $context = context_system::instance();
 require_login();
@@ -8,21 +9,22 @@ $PAGE->set_context($context);
 $PAGE->set_url($CFG->wwwroot.'/local/learningcompanions/mentor/mentor_questions_overview.php');
 $PAGE->set_pagelayout('standard');
 $PAGE->requires->js_call_amd('local_learningcompanions/mentor', 'init');
-$PAGE->requires->css('/local/learningcompanions/vendor/DataTables/datatables.min.css');
-$PAGE->requires->css('/local/learningcompanions/vendor/balloon.css');
+$PAGE->requires->css('/local/learningcompanions/js_lib/DataTables/datatables.min.css');
+$PAGE->requires->css('/local/learningcompanions/js_lib/balloon.css');
 $PAGE->navbar->add(get_string('navbar_mentors', 'local_learningcompanions'));
 $PAGE->navbar->add(get_string('navbar_mentorquestions', 'local_learningcompanions'), new moodle_url('/local/learningcompanions/mentor/index.php'));
 
-$questionsToMe = \local_learningcompanions\mentors::get_all_mentor_questions($USER->id,  null, false, true);
+$questionsToMe = \local_learningcompanions\mentors::get_mentor_questions_by_user_id($USER->id);
 $questionsToMe = array_values($questionsToMe);
 $myTopics = \local_learningcompanions\mentors::get_mentorship_topics($USER->id);
-$questionsToAllMentors = \local_learningcompanions\mentors::get_all_mentor_questions(null, $myTopics, false, true);
+$questionsToAllMentors = \local_learningcompanions\mentors::get_mentor_questions_by_topics($myTopics);
 $questionsToAllMentors = array_values($questionsToAllMentors);
 $hasQuestionsToMe = count($questionsToMe) > 0;
 $hasQuestionsToAllMentors = count($questionsToAllMentors) > 0;
 $learningNuggetComments = \local_learningcompanions\mentors::get_learning_nugget_comments();
 $learningNuggetComments = array_values($learningNuggetComments);
 $hasComments = count($learningNuggetComments) > 0;
+
 echo $OUTPUT->header();
 echo $OUTPUT->render_from_template('local_learningcompanions/mentor/mentor_questions_overview', array(
     'questionstome' => $questionsToMe,
@@ -33,6 +35,5 @@ echo $OUTPUT->render_from_template('local_learningcompanions/mentor/mentor_quest
     'hascomments' => $hasComments,
     'cfg' => $CFG
 ));
-
 echo $OUTPUT->footer();
 
