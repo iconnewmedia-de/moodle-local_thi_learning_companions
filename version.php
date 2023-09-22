b0de938a132e49812adf9bbb95a6802437f66e03
Pascal Collins <pascal.collins@iconnewmedia.de>
2023-09-22 10:01:28 +0200

    increases version numbers

--- version.php
+++ version.php
@@ -25,6 +25,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_learningcompanions';
-$plugin->release = '0.1.0';
-$plugin->version = 2023090402;
+$plugin->release = '1.0.0';
+$plugin->version = 2023092200;
 $plugin->requires = 2019111800;
